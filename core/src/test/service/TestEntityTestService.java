fdd1a2564e5fb386d6f8575359064ee3e787e42a
jeremy <jeremy@zhangzhide.com>
2016-08-04 00:07:24 +0800

    1

---
+++ core/src/test/service/TestEntityTestService.java
@@ -0,0 +1,10 @@
+package service;
+
+import org.junit.runner.RunWith;
+
+/**
+ * Created by jeremy on 2016/8/3.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+public class TestEntityTestService {
+}
